c7f1a0c7c595d4491a92d9a89a52e824d0c2ce4e
Omar Sahl <omar753sahl@gmail.com>
2016-11-24 01:57:40 +0200

    added some dependencies. finished some UI components.

---
+++ src/main/java/com/hashlearning/gui/controllers/DashboardController.java
@@ -0,0 +1,21 @@
+package com.hashlearning.gui.controllers;
+
+import com.jfoenix.controls.JFXListView;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Bounds;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashboardController implements Initializable {
+
+    @FXML
+    private JFXListView<?> list_view;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+}
--- src/main/java/com/hashlearning/gui/screens/LoginScreen.java
+++ src/main/java/com/hashlearning/gui/screens/LoginScreen.java
@@ -5,7 +5,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -20,12 +19,11 @@ public class LoginScreen extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/landing_page.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/dashboard_page.fxml"));
         Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("/css/main_stylesheet.css").toExternalForm());
         StageInitializer.initialize(primaryStage);
         primaryStage.setScene(scene);
         primaryStage.show();
-
-
     }
 }

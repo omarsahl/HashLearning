78d63463c9598122a70e69b04474f7059c4499d5
ahmed-ayman <terabyte055@gmail.com>
2016-11-28 11:34:56 +0200

    added Encryption class

---
+++ src/main/java/com/hashlearning/utils/Encrypt.java
@@ -0,0 +1,34 @@
+package com.hashlearning.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by ahmed-ayman on 11/28/16.
+ */
+public class Encrypt {
+    public static String encrypt(String password){
+     return get_SHA_512_SecurePassword(password,"RAND")   ;
+    }
+
+    private static String get_SHA_512_SecurePassword(String passwordToHash, String salt){
+        String generatedPassword = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            md.update(salt.getBytes("UTF-8"));
+            byte[] bytes = md.digest(passwordToHash.getBytes("UTF-8"));
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++){
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            generatedPassword = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return generatedPassword;
+    }
+}

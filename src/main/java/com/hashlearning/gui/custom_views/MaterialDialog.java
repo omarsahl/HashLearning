688024c3a16deff0d02b3fcdf2092497d03e8ba2
Omar Sahl <omar753sahl@gmail.com>
2016-12-09 21:20:14 +0200

    Rebuilt the DatabaseManager class using JSON as a noSql database.

--- src/main/java/com/hashlearning/gui/custom_views/MaterialDialog.java
+++ src/main/java/com/hashlearning/gui/custom_views/MaterialDialog.java
@@ -2,6 +2,8 @@ package com.hashlearning.gui.custom_views;
 
 import javafx.scene.control.Alert;
 import javafx.scene.control.DialogPane;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
 /**
  * Created by Omar on 09-Dec-16
@@ -20,6 +22,9 @@ public class MaterialDialog {
         DialogPane dialogPane = alert.getDialogPane();
         dialogPane.getStylesheets().add(MaterialDialog.class.getResource("/css/material_dialog_stylesheet.css").toExternalForm());
         dialogPane.getStyleClass().add("material-dialog");
+
+        Stage dialogStage = (Stage) dialogPane.getScene().getWindow();
+        dialogStage.getIcons().add(new Image(MaterialDialog.class.getResource("/HL_hash_logo.png").toString()));
 
         alert.showAndWait();
 
